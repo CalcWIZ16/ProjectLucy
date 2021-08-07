7e4fdbf9207d6fbd3bc07b919154f78871eb6b38
ankleBowl <specialreeree6@gmail.com>
2021-08-06 22:42:18 -0700

    Refactored the code to make it easier to add

--- LucyMint1/lucy.py
+++ LucyMint1/lucy.py
@@ -8,64 +8,22 @@ from spotipy.oauth2 import SpotifyOAuth
 from config import *
 from speak import *
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SP_CI, client_secret=SP_CS, redirect_uri=SP_URL, scope=SCOPE))
+import spotify as spotify
 
 def tts(string):
     i = 0
     speakText(string)
 
 def parseQuestion(string):
-    try:
-        userIn = string
-        userIn = userIn.lower()
-        print("<"+your_name+"> "+userIn)
+    userIn = string.lower()
+    print("<"+your_name+"> "+userIn)
 
-        #Gimmics
-        if 'hello' in userIn:
-            tts("Hello "+your_name)
+    #Gimmics
+    if 'hello' in userIn:
+        tts("Hello "+your_name)
 
-        if 'tell' in userIn and 'joke' in userIn:
-            tts(pyjokes.get_joke())
+    if 'tell' in userIn and 'joke' in userIn:
+        tts(pyjokes.get_joke())
 
-        # SPOTIFY CONTROL
-        # Pause
-        elif userIn == 'exit':
-            activated = False
-        elif userIn == 'pause' or userIn == 'stop':
-            sp.pause_playback()
-        # Resume
-        elif userIn == 'resume':
-            sp.start_playback()
-        # Next Song
-        elif userIn == 'skip':
-            sp.next_track()
-            tts('skipping')
-        # Previous Song
-        elif userIn == 'previous':
-            sp.previous_track()
-            tts('previous')
-        # Play Song
-        elif 'play' in userIn:
-            userArray = userIn.split(" ")
-            if len(userArray) > 1:
-                song = userIn.replace('play ', '')
-                # tts("I'm on it")
-                # print('On it!')
-
-                response = sp.search(q=song, type="track")
-
-                artist = response["tracks"]["items"][0]["artists"][0]["uri"]
-                album = response["tracks"]["items"][0]["album"]["uri"]
-                track = response["tracks"]["items"][0]["uri"]
-                name = response["tracks"]["items"][0]["name"]
-
-                tts("Playing " + name)
-                print('<Lucy> Playing "' + name + '"')
-
-                sp.start_playback(context_uri=album , offset={"uri": track})
-            else:
-                sp.start_playback()
-
-    except:
-        speakError()
-        print("<Lucy> Sorry, I ran into a problem")
+    if 'on spotify' in userIn:
+        spotify.parseQuestion(userIn)
---
+++ LucyMint1/spotify.py
@@ -0,0 +1,51 @@
+# play ... on spotify (as input)
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from config import *
+from speak import *
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SP_CI, client_secret=SP_CS, redirect_uri=SP_URL, scope=SCOPE))
+
+def parseQuestion(string):
+    userIn = string.removesuffix(' on spotify')
+    try:
+        # SPOTIFY CONTROL
+        # Pause
+        if userIn == 'pause' or userIn == 'stop':
+            sp.pause_playback()
+        # Resume
+        elif userIn == 'resume':
+            sp.start_playback()
+        # Next Song
+        elif userIn == 'skip':
+            sp.next_track()
+            speakText('skipping')
+        # Previous Song
+        elif userIn == 'previous':
+            sp.previous_track()
+            speakText('previous')
+        # Play Song
+        elif 'play' in userIn:
+            userArray = userIn.split(" ", 1)
+            if len(userArray) > 1:
+                print(userArray[1])
+                song = userArray[1]
+
+                response = sp.search(q=song, type="track")
+
+                artist = response["tracks"]["items"][0]["artists"][0]["uri"]
+                album = response["tracks"]["items"][0]["album"]["uri"]
+                track = response["tracks"]["items"][0]["uri"]
+                name = response["tracks"]["items"][0]["name"]
+
+                speakText("Playing " + name)
+                print('<Lucy> Playing "' + name + '"')
+
+                sp.start_playback(context_uri=album , offset={"uri": track})
+            else:
+                sp.start_playback()
+
+    except:
+        speakError()
+        print("<Lucy> Sorry, I ran into a problem")
