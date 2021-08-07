22fb2c5fc0a7602c160063d17f9e2f1ca823e650
ankleBowl <specialreeree6@gmail.com>
2021-08-06 19:10:31 -0700

    Fixed it

---
+++ LucyMint1/lucy.py
@@ -0,0 +1,65 @@
+import requests
+import speech_recognition as sr
+# import pyttsx3
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from config import *
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SP_CI, client_secret=SP_CS, redirect_uri=SP_URL, scope=SCOPE))
+
+def tts(string):
+    i = 0
+
+while True:
+    try:
+        with sr.Microphone() as source:
+            # print("<Lucy> I'm Listening...")
+            # audio = r.listen(source)
+            # userIn = r.recognize_google(audio)
+            userIn = input("I'm listening: ")
+            print("<"+your_name+"> "+userIn)
+
+            if 'hello Lucy' in userIn:
+                tts("Hello "+your_name)
+
+            # SPOTIFY CONTROL
+            # Pause
+            elif userIn == 'pause' or userIn == 'stop':
+                sp.pause_playback()
+            # Resume
+            elif userIn == 'resume':
+                sp.start_playback()
+            # Next Song
+            elif userIn == 'skip':
+                sp.next_track()
+                tts('skipping')
+            # Previous Song
+            elif userIn == 'previous':
+                sp.previous_track()
+                tts('previous')
+            # Play Song
+            elif 'play' in userIn:
+                userArray = userIn.split(" ")
+                if len(userArray) > 1:
+                    song = userIn.replace('play ', '')
+                    tts("I'm on it")
+                    print('On it!')
+
+                    response = sp.search(q=song, type="track")
+
+                    artist = response["tracks"]["items"][0]["artists"][0]["uri"]
+                    album = response["tracks"]["items"][0]["album"]["uri"]
+                    track = response["tracks"]["items"][0]["uri"]
+                    name = response["tracks"]["items"][0]["name"]
+
+                    tts("Playing " + name)
+                    print('<Lucy> Playing "' + name + '"')
+
+                    sp.start_playback(context_uri=album , offset={"uri": track})
+                else:
+                    sp.start_playback()
+
+    except:
+        tts("Sorry, I didn't quite catch that")
+        print("<Lucy> Sorry, I didn't quite catch that")
+        pass
---
+++ LucyMint1/speak.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/tts-for-messages-354f8a743220.json'
