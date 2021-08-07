30bab5160fb23341bfc8c69de128f22982b751a1
ankleBowl <specialreeree6@gmail.com>
2021-08-07 15:07:13 -0700

    Fixed gitignore + added voiceline caching

--- LucyMint1/speak.py
+++ LucyMint1/speak.py
@@ -29,16 +29,17 @@ audio_config = texttospeech.AudioConfig(
 
 
 def speakText(string):
-    synthesis_input = texttospeech.SynthesisInput(text=string)
-    response = client.synthesize_speech(
-        input=synthesis_input, voice=voice, audio_config=audio_config
-    )
-    # The response's audio_content is binary.
-    with open("output.mp3", "wb") as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-    playsound('output.mp3')
+    if not os.path.isfile("voice/" + string + ".mp3"):
+        synthesis_input = texttospeech.SynthesisInput(text=string)
+        response = client.synthesize_speech(
+            input=synthesis_input, voice=voice, audio_config=audio_config
+        )
+        # The response's audio_content is binary.
+        with open("voice/" + string + ".mp3", "wb") as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file "output.mp3"')
+    playsound("voice/" + string + '.mp3')
 
 def speakError():
     playsound('error.mp3')
